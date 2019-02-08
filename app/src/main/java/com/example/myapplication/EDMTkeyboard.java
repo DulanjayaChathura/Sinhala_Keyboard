659a613884ee919837ceff60ec89dfd1a0679018
DulanjayaChathura <sacdulanjaya.16@cse.mrt.ac.lk>
2019-02-09 00:02:53 +0530

    keyboard with sinhala keys

--- app/src/main/java/com/example/myapplication/EDMTkeyboard.java
+++ app/src/main/java/com/example/myapplication/EDMTkeyboard.java
@@ -1,32 +1,44 @@
 package com.example.myapplication;
 
-import android.app.Service;
-import android.content.Intent;
 import android.inputmethodservice.InputMethodService;
 import android.inputmethodservice.Keyboard;
 import android.inputmethodservice.KeyboardView;
-import android.os.IBinder;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.InputConnection;
+import android.view.inputmethod.InputMethodSubtype;
 
-public class EDMTkeyboard extends InputMethodService implements KeyboardView.OnKeyboardActionListener {
 
-    private KeyboardView kv;
-    private Keyboard keyboard;
-    private boolean isCaps=false;
+public class EDMTkeyboard extends InputMethodService implements KeyboardView.OnKeyboardActionListener{
+
+    private KeyboardView inputView;
+    private Keyboard sinhalaKeyboard;//keyboard1 when caps off
+
+    private Keyboard sinhalaKeyboardShift; //keyboard1 when caps on
+
+   // public static String activeKeyboard;
+    //private Keyboard curKeyboard;
+
+
+
+    @Override
+    public void onInitializeInterface(){
+        sinhalaKeyboard=new Keyboard(this,R.xml.keyboard1);
+        sinhalaKeyboardShift=new Keyboard(this,R.xml.keyboard2);
+        //making new keyboard1
+    }
+
 
     @Override
     public View onCreateInputView() {
-        kv=(KeyboardView)getLayoutInflater().inflate(R.layout.keyboardlayout,null);
+        onInitializeInterface();
+        inputView=(KeyboardView)getLayoutInflater().inflate(R.layout.keyboardlayout,null);
         //instantiate a layout XML file into its corresponding view object
-        keyboard=new Keyboard(this,R.xml.keyboard);
-        //making new keyboard
-        kv.setKeyboard(keyboard);
-        kv.setOnKeyboardActionListener(this);
-        //connect to this class reference
-        return kv;
+        inputView.setKeyboard(sinhalaKeyboard);
 
+        inputView.setOnKeyboardActionListener(this);
+
+     return inputView;
     }
 
     @Override
@@ -46,16 +58,15 @@ public class EDMTkeyboard extends InputMethodService implements KeyboardView.OnK
     //send a key press to listener
         InputConnection ic=getCurrentInputConnection();
         //Retrieve the currently active InputConnection that is bound to the input method, or null if there is none.
-        playClick(primaryCode);//if want to add sounds
+        //playClick(primaryCode);//if want to add sounds
         //switch case statement
         switch(primaryCode) {
             case Keyboard.KEYCODE_DELETE:
                 ic.deleteSurroundingText(1, 0);
                 break;//we delete only one character at a time
             case Keyboard.KEYCODE_SHIFT:
-                isCaps = !isCaps;
-                keyboard.setShifted(isCaps);
-                kv.invalidateAllKeys();//Requests a redraw of the entire keyboard.
+                inputView.invalidateAllKeys();//Requests a redraw of the entire keyboard1.
+                handleShift();
                 break;
             case Keyboard.KEYCODE_DONE:
                 ic.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_ENTER));
@@ -63,21 +74,50 @@ public class EDMTkeyboard extends InputMethodService implements KeyboardView.OnK
                 break;
             default:
                 char code = (char) primaryCode;
-                if (Character.isLetter(code) && isCaps)
-                    code = Character.toUpperCase(code);//set letter to upper case
+                //if (Character.isLetter(code) && isCaps)
+                 // code = Character.toUpperCase(code);//set letter to upper case
                 ic.commitText(String.valueOf(code), 1);
         }
+    }
+    private void handleShift(){
 
 
+        Keyboard currentKeyboard=inputView.getKeyboard();
+        if (currentKeyboard==sinhalaKeyboard){
+            sinhalaKeyboard.setShifted(true);
+            inputView.setKeyboard(sinhalaKeyboardShift);
+        }else{
+            sinhalaKeyboardShift.setShifted(true);
+            inputView.setKeyboard(sinhalaKeyboard);
+        }
+    }
 
+    /**
+     * Switch to language when it is changed from Choose Input Method.
+
+    @Override
+    public void onCurrentInputMethodSubtypeChanged(InputMethodSubtype subtype) {
+       // inputView.setSubtypeOnSpaceKey(subtype);
+        String s = subtype.getLocale();
+        switch (s) {
+            case "Sinhala":
+                activeKeyboard = "Sinhala";
+                curKeyboard = sinhalaKeyboard;
+                break;
+            case "SinhalaShift":
+                activeKeyboard = "SinhalaShift";
+                curKeyboard = sinhalaKeyboardShift;
+                break;
+
+        }
+
+        // Apply the selected keyboard to the input view.
+        inputView.setKeyboard(curKeyboard);
     }
 
 
+*/
 
-
-    private void playClick(int primaryCode) {
-        //add sound
-    }
 
     @Override
     public void onText(CharSequence text) {
@@ -103,4 +143,5 @@ public class EDMTkeyboard extends InputMethodService implements KeyboardView.OnK
     public void swipeUp() {
     //Called when the user quickly moves the finger from down to up.
     }
+
 }
