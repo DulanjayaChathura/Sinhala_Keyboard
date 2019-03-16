c6760c1069cf252a17d130d0bbc14d76a30af066
DulanjayaChathura <sacdulanjaya.16@cse.mrt.ac.lk>
2019-03-16 09:00:33 +0530

    candidate view completed for one word

--- app/src/main/java/com/example/myapplication/SinhalaKeyboard.java
+++ app/src/main/java/com/example/myapplication/SinhalaKeyboard.java
@@ -3,23 +3,55 @@ package com.example.myapplication;
 import android.inputmethodservice.InputMethodService;
 import android.inputmethodservice.Keyboard;
 import android.inputmethodservice.KeyboardView;
+import android.text.method.MetaKeyKeyListener;
+import android.util.Log;
+import android.view.KeyCharacterMap;
 import android.view.KeyEvent;
-import android.view.LayoutInflater;
 import android.view.View;
+import android.view.inputmethod.CompletionInfo;
 import android.view.inputmethod.InputConnection;
-import android.view.inputmethod.InputMethodSubtype;
+import android.view.inputmethod.InputMethodManager;
+import android.view.textservice.TextInfo;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class SinhalaKeyboard extends InputMethodService implements KeyboardView.OnKeyboardActionListener{
 
     private KeyboardView inputView;
-    private Candidate_view candidateView;
     private Keyboard sinhalaKeyboard;//keyboard1 when caps off
     private Keyboard symbolKeyboard;// symbol keyboard
     private Keyboard symbolShiftKeyboard; //symbol shift keyboard
     private Keyboard sinhalaKeyboardShift; //keyboard1 when caps on
     private String  type="language";// to keep a track of type whether it is symbol or language
     private boolean isCap=false;
+
+    /**keyboard  **/
+
+    private InputMethodManager mInputMethodManager;
+    private Candidate_view mCandidateView;
+    private CompletionInfo[] mCompletions;
+
+    private StringBuilder mComposing = new StringBuilder();
+    private boolean mPredictionOn=true;
+    private boolean mCompletionOn;
+    private boolean mSound;
+    private int mLastDisplayWidth;
+    private boolean mCapsLock;
+    private long mLastShiftTime;
+    private long mMetaState;
+    private String mWordSeparators;
+    private ArrayList<String> list;
+    private String typedWord="";
+
+
+
+
+
+    /**keyboard  **/
+
+
 
    // public static String activeKeyboard;
     //private Keyboard curKeyboard;
@@ -48,29 +80,310 @@ public class SinhalaKeyboard extends InputMethodService implements KeyboardView.
      return inputView;
     }
 
-    @Override
-
-    public void onPress(int primaryCode) {
-    //called when the user pressed a key
-
-    }
 
     @Override
     public void onRelease(int primaryCode){
     //Called when the user releases a key.
     }
+
+
     /**
      * Called by the framework when your view for showing candidates needs to
      * be generated, like {@link #onCreateInputView}.
 
+     **/
+    /** keyboard test**/
+
     @Override
     public View onCreateCandidatesView() {
-        candidateView=(Candidate_view)getLayoutInflater().inflate(R.layout.candidate_view,null);
+        mCandidateView = new Candidate_view(this);
+        mCandidateView.setService(this);
 
-        candidateView.setService(this);
-        return candidateView;
+        return mCandidateView;
     }
+
+    /**
+     * Deal with the editor reporting movement of its cursor.
      */
+    @Override
+    public void onUpdateSelection(int oldSelStart, int oldSelEnd,
+                                  int newSelStart, int newSelEnd,
+                                  int candidatesStart, int candidatesEnd) {
+        super.onUpdateSelection(oldSelStart, oldSelEnd, newSelStart, newSelEnd,
+                candidatesStart, candidatesEnd);
+
+        // If the current selection in the text view changes, we should
+        // clear whatever candidate text we have.
+        if (mComposing.length() > 0 && (newSelStart != candidatesEnd
+                || newSelEnd != candidatesEnd)) {
+            mComposing.setLength(0);
+            updateCandidates();
+            InputConnection ic = getCurrentInputConnection();
+            if (ic != null) {
+                ic.finishComposingText();
+            }
+        }
+    }
+
+    /**
+     * This tells us about completions that the editor has determined based
+     * on the current text in it.  We want to use this in fullscreen mode
+     * to show the completions ourselves, since the editor can not be seen
+     * in that situation.
+     */
+    @Override
+    public void onDisplayCompletions(CompletionInfo[] completions) {
+        if (mCompletionOn) {
+            mCompletions = completions;
+            if (completions == null) {
+                setSuggestions(null, false, false);
+                return;
+            }
+
+            List<String> stringList = new ArrayList<>();
+            for (CompletionInfo ci : completions) {
+                if (ci != null) stringList.add(ci.getText().toString());
+            }
+            setSuggestions(stringList, true, true);
+        }
+    }
+
+    /**
+     * This translates incoming hard key events in to edit operations on an
+     * InputConnection.  It is only needed when using the
+     * PROCESS_HARD_KEYS option.
+     */
+    private boolean translateKeyDown(int keyCode, KeyEvent event) {
+        mMetaState = MetaKeyKeyListener.handleKeyDown(mMetaState,
+                keyCode, event);
+        int c = event.getUnicodeChar(MetaKeyKeyListener.getMetaState(mMetaState));
+        mMetaState = MetaKeyKeyListener.adjustMetaAfterKeypress(mMetaState);
+        InputConnection ic = getCurrentInputConnection();
+        if (c == 0 || ic == null) {
+            return false;
+        }
+
+        if ((c & KeyCharacterMap.COMBINING_ACCENT) != 0) {
+            c = c & KeyCharacterMap.COMBINING_ACCENT_MASK;
+        }
+
+        if (mComposing.length() > 0) {
+            char accent = mComposing.charAt(mComposing.length() - 1);
+            int composed = KeyEvent.getDeadChar(accent, c);
+            if (composed != 0) {
+                c = composed;
+                mComposing.setLength(mComposing.length() - 1);
+            }
+        }
+
+        onKey(c, null);
+
+        return true;
+    }
+
+
+    public void setSuggestions(List<String> suggestions, boolean completions,
+                               boolean typedWordValid) {
+        if (suggestions != null && suggestions.size() > 0) {
+            setCandidatesViewShown(true);
+        } else if (isExtractViewShown()) {
+            setCandidatesViewShown(true);
+        }
+        if (mCandidateView != null) {
+
+            mCandidateView.setSuggestions(suggestions, completions, typedWordValid);
+        }
+    }
+/*
+    // Tap on suggestion to commit
+    public void pickSuggestionManually(int index) {
+        if (mCompletionOn && mCompletions != null && index >= 0 && index < mCompletions.length) {
+            CompletionInfo ci = mCompletions[index];
+            getCurrentInputConnection().commitCompletion(ci);
+            if (mCandidateView != null) {
+                mCandidateView.clear();
+            }
+
+        } else if (mComposing.length() > 0) {
+            // If we were generating candidate suggestions for the current
+            // text, we would commit one of them here. But for this sample,
+            // we will just commit the current text.
+            mComposing.setLength(index);
+            mComposing = new StringBuilder(list.get(index) + " ");
+
+        }
+    }*/
+
+
+    public void onPress(int primaryCode) {
+        inputView.setPreviewEnabled(true);
+
+        // Disable preview key on Shift, Delete, Space, Language, Symbol and Emoticon.
+        if (primaryCode == -1 || primaryCode == -5 || primaryCode == -2 || primaryCode == -10000
+                || primaryCode == -101 || primaryCode == 32) {
+            inputView.setPreviewEnabled(false);
+        }
+    }
+    /**
+     * Helper to send a key down / key up pair to the current editor.
+     */
+    private void keyDownUp(int keyEventCode) {
+        getCurrentInputConnection().sendKeyEvent(
+                new KeyEvent(KeyEvent.ACTION_DOWN, keyEventCode));
+        getCurrentInputConnection().sendKeyEvent(
+                new KeyEvent(KeyEvent.ACTION_UP, keyEventCode));
+    }
+
+    /**
+     * Helper to send a character to the editor as raw key events.
+     */
+    private void sendKey(int keyCode) {
+        switch (keyCode) {
+            case '\n':
+                keyDownUp(KeyEvent.KEYCODE_ENTER);
+                break;
+            default:
+                if (keyCode >= '0' && keyCode <= '9') {
+                    keyDownUp(keyCode - '0' + KeyEvent.KEYCODE_0);
+                } else {
+                    getCurrentInputConnection().commitText(String.valueOf((char) keyCode), 1);
+                }
+                break;
+        }
+    }
+
+    /**
+     * Helper function to commit any text being composed in to the editor.
+     */
+
+
+    private String getWordSeparators() {
+        mWordSeparators="!1234567890 @#$%&*-=()!\"':;/?«~±×÷•°`´{}©£€^®¥_+[]¡<>¢|\\¿»";
+        return mWordSeparators;
+    }
+
+    public boolean isWordSeparator(int code) {
+        String separators = getWordSeparators();
+        return separators.contains(String.valueOf((char)code));
+    }
+
+//    private void handleCharacter(int primaryCode, int[] keyCodes) {
+//        if (isInputViewShown()) {
+//            if (mInputView.isShifted()) {
+//                primaryCode = Character.toUpperCase(primaryCode);
+//            }
+//        }
+//        if (mPredictionOn) {
+//            mComposing.append((char) primaryCode);
+//            getCurrentInputConnection().setComposingText(mComposing, 1);
+//            updateShiftKeyState(getCurrentInputEditorInfo());
+//            updateCandidates();
+//        } else {
+//            getCurrentInputConnection().commitText(
+//                    String.valueOf((char) primaryCode), 1);
+//        }
+//    }
+
+    private void handleCharacter(int primaryCode, int[] keyCodes) {
+
+
+
+       getCurrentInputConnection().commitText(String.valueOf((char)primaryCode),1);
+        if (isWordSeparator(primaryCode)) {
+            typedWord="";
+
+            //  updateShiftKeyState(getCurrentInputEditorInfo());
+        } else {
+            typedWord+=(String.valueOf((char)primaryCode));
+
+
+
+        }
+        updateCandidates();
+    }
+
+
+    /**
+     * Update the list of available candidates from the current composing
+     * text.  This will need to be filled in by however you are determining
+     * candidates.
+     */
+    private void updateCandidates() {
+//        if (!mCompletionOn) {
+//            if (mComposing.length() > 0) {
+//                ArrayList<String> list = new ArrayList<String>();
+//                list.add(mComposing.toString());
+//                Log.d("SoftKeyboard", "REQUESTING: " + mComposing.toString());
+//                //mScs.getSentenceSuggestions(new TextInfo[] {new TextInfo(mComposing.toString())}, 5);
+//                setSuggestions(list, true, true);
+//            } else {
+//                setSuggestions(null, false, false);
+//            }
+//        }
+
+        if (!mCompletionOn) {
+            try{
+
+                if (typedWord.length() > 0) {
+                    ArrayList<String> list = new ArrayList<String>();
+                    list.add(typedWord);
+                    Log.d("SoftKeyboard", "REQUESTING: " + typedWord);
+                    // Log.d("keyboard","candidate"+ typedWord);
+                    //mScs.getSentenceSuggestions(new TextInfo[] {new TextInfo(mComposing.toString())}, 5);
+                    setSuggestions(list, true, true);
+                } else {
+                    setSuggestions(null, false, false);
+                }
+
+            }
+            catch(NullPointerException e){
+
+            }
+
+
+        }
+    }
+    private void handleBackspace() {
+/*        final int length = mComposing.length();
+        if (length > 1) {
+            mComposing.delete(length - 1, length);
+            getCurrentInputConnection().setComposingText(mComposing, 1);
+            updateCandidates();
+        } else if (length > 0) {
+            mComposing.setLength(0);
+            getCurrentInputConnection().commitText("", 0);
+            updateCandidates();
+        }
+        else{
+
+        }*/
+        getCurrentInputConnection().deleteSurroundingText(1,0);// delete surrounding text
+        if(typedWord.length()>0) {
+            typedWord = typedWord.substring(0, typedWord.length() - 1);
+            updateCandidates();
+        }
+    }
+
+    /**
+     * This is called when the user is done editing a field.  We can use
+     * this to reset our state.
+     */
+    @Override public void onFinishInput() {
+        super.onFinishInput();
+
+        // Clear current composing text and candidates.
+        mComposing.setLength(0);
+        updateCandidates();
+
+        // We only hide the candidates window when finishing input on
+        // a particular editor, to avoid popping the underlying application
+        // up and down if the user is entering text into the bottom of
+        // its window.
+        setCandidatesViewShown(false);
+
+
+    }
+    /** keyboard test**/
     @Override
     public void onKey(int primaryCode, int[] keyCodes) {
     //send a key press to listener
@@ -80,7 +393,7 @@ public class SinhalaKeyboard extends InputMethodService implements KeyboardView.
         //switch case statement
         switch(primaryCode) {
             case Keyboard.KEYCODE_DELETE:
-                ic.deleteSurroundingText(1, 0);
+                handleBackspace();
                 break;//we delete only one character at a time
             case Keyboard.KEYCODE_SHIFT:
                 inputView.invalidateAllKeys();//Requests a redraw of the entire keyboard1.
@@ -95,13 +408,18 @@ public class SinhalaKeyboard extends InputMethodService implements KeyboardView.
                 //KeyEvent(int action, int code(relevant code))
                 break;
             default:
-                char code = (char) primaryCode;
-                //if (Character.isLetter(code) && isCaps)
-                 // code = Character.toUpperCase(code);//set letter to upper case
-                ic.commitText(String.valueOf(code), 1);
+           /* if (isWordSeparator(primaryCode)){
+                // Handle separator
+                if (mComposing.length() > 0) {
+                    commitTyped(getCurrentInputConnection());
+                }
+                sendKey(primaryCode);
+                //updateShiftKeyState(getCurrentInputEditorInfo());
+            }*/
+                handleCharacter(primaryCode, keyCodes);
         }
     }
-    private void handleShift(){
+    private void handleShift(){// handle the shift key of the keybaord
 
 
         Keyboard currentKeyboard=inputView.getKeyboard();
@@ -157,33 +475,19 @@ public class SinhalaKeyboard extends InputMethodService implements KeyboardView.
            }
        }
     }
+/**
+    public void setSuggetion(List<String> suggetion, boolean completion, boolean typeWordValid){
 
-
-    /**
-     * Switch to symbols when it is changed from Choose Input Method.
-
-    @Override
-    public void onCurrentInputMethodSubtypeChanged(InputMethodSubtype subtype) {
-       // inputView.setSubtypeOnSpaceKey(subtype);
-        String s = subtype.getLocale();
-        switch (s) {
-            case "Sinhala":
-                activeKeyboard = "Sinhala";
-                curKeyboard = sinhalaKeyboard;
-                break;
-            case "SinhalaShift":
-                activeKeyboard = "SinhalaShift";
-                curKeyboard = sinhalaKeyboardShift;
-                break;
-
+        if((suggetion != null & suggetion.size()>0)|(isExtractViewShown())){
+            setCandidatesViewShown(true);
+        }
+        if(candidateView != null){
+            candidateView.setSuggetion(suggetion,completion,typeWordValid);
         }
 
-        // Apply the selected keyboard to the input view.
-        inputView.setKeyboard(curKeyboard);
     }
+**/
 
-
-*/
 
 
     @Override
